4b809bef020e0499d4f022043361e844182bee56
Ajay Paul <ajaypaul@Ajays-MacBook-Air.local>
2024-11-13 20:27:35 +0530

    resolved

--- src/Components/common/Sidebar.jsx
+++ src/Components/common/Sidebar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { Nav } from "react-bootstrap";
 import { BiChevronDown } from "react-icons/bi";
@@ -41,33 +40,11 @@ const Sidebar = () => {
             as={Link}
             to="/"
             className="btn btn-secondary text-white d-flex align-items-center"
-=======
-import React from "react";
-import { Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
-
-const Sidebar = () => {
-  return (
-    <div className="Sidebar">
-      <div className="d-flex flex-column bg-white border border-dashed text-black vh-100 p-3">
-        {/* App Title */}
-        <Nav.Link as={Link} to="/" className="display-1 text-center text-black">
-          SPACIFYD
-        </Nav.Link>
-
-        {/* Navigation Links */}
-        <Nav className="flex-column mt-4">
-          <Nav.Link
-            as={Link} // Use as={Link} to apply Link behavior
-            to="/" // Use "to" for the path instead of "href"
-            className="btn btn-secondary text-black d-flex align-items-center"
->>>>>>> ab8b85bc1554dbb9eb706d0507dc490e5db36073
           >
             <i className="bi bi-house-door me-2"></i>
             Home
           </Nav.Link>
 
-<<<<<<< HEAD
           {/* Categories Button with Toggle */}
           <Nav.Link
             as="button"
@@ -109,57 +86,28 @@ const Sidebar = () => {
             as={Link}
             to="/products"
             className="btn btn-secondary text-white d-flex align-items-center"
-=======
-          <Nav.Link
-            as={Link}
-            to="/categories"
-            className="btn btn-secondary text-black d-flex align-items-center"
-          >
-            <i className="bi bi-grid me-2"></i>
-            Categories
-          </Nav.Link>
-
-          <Nav.Link
-            as={Link}
-            to="/products"
-            className="btn btn-secondary text-black d-flex align-items-center"
->>>>>>> ab8b85bc1554dbb9eb706d0507dc490e5db36073
             style={{ whiteSpace: "nowrap" }}
           >
             <i className="bi bi-box me-2"></i>
             All Products
           </Nav.Link>
 
-<<<<<<< HEAD
           {/* All Projects Link */}
           <Nav.Link
             as={Link}
             to="/projects"
             className="btn btn-secondary text-white d-flex align-items-center"
-=======
-          <Nav.Link
-            as={Link}
-            to="/projects"
-            className="btn btn-secondary text-black d-flex align-items-center"
->>>>>>> ab8b85bc1554dbb9eb706d0507dc490e5db36073
             style={{ whiteSpace: "nowrap" }}
           >
             <i className="bi bi-clipboard-data me-2"></i>
             All Projects
           </Nav.Link>
 
-<<<<<<< HEAD
           {/* Brands Link */}
           <Nav.Link
             as={Link}
             to="/brands"
             className="btn btn-secondary text-white d-flex align-items-center"
-=======
-          <Nav.Link
-            as={Link}
-            to="/brands"
-            className="btn btn-secondary text-black d-flex align-items-center"
->>>>>>> ab8b85bc1554dbb9eb706d0507dc490e5db36073
           >
             <i className="bi bi-tag me-2"></i>
             Brands
