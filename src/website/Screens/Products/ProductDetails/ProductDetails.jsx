ff8d677b66c34a4660b71f56a9f619a9ba4e2a50
dev-dennis-d <dennisdaniel2002@gmail.com>
2025-03-05 21:40:21 +0530

    Updated products and details page

--- src/website/Screens/Products/ProductDetails/ProductDetails.jsx
+++ src/website/Screens/Products/ProductDetails/ProductDetails.jsx
@@ -7,14 +7,82 @@ const ProductDetails = () => {
   return (
     <div className="main">
       <HomeNavbar></HomeNavbar>
-      <div>
-        <HeroImage></HeroImage>{" "}
+      {/* Hero Section with Background */}
+      <div
+        className="services-container"
+        style={{ backgroundImage: `url(${HeroImage})` }}
+      >
+        {/* <div className="container px-4">
+          <h1 className="service-title">Our Products</h1>
+          <p className="service-description">
+            Explore our curated range of premium products designed to transform
+            every <br /> corner of your space.
+          </p>
+        </div> */}
       </div>
-      <div className="similar-products container ">
-        <span>
+      <div>{/* <HeroImage></HeroImage>{" "} */}</div>
+
+      <div className=" container similar-products ">
+        <h2>Similar Products</h2>
+        <div>
           {" "}
-          <p>Similar Products</p>
-        </span>
+          <button>Veiw More</button>
+        </div>
+      </div>
+      <div className="p-4">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+          <div class="col">
+            <div class="card">
+              {/* <img src="..." class="card-img-top" alt="..."> */}
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              {/* <img src="..." class="card-img-top" alt="..."> */}
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              {/* <img src="..." class="card-img-top" alt="..."> */}
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              {/* <img src="..." class="card-img-top" alt="..."> */}
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <Footer></Footer>
     </div>
--- src/website/Screens/Products/ProductList/Products.jsx
+++ src/website/Screens/Products/ProductList/Products.jsx
@@ -11,9 +11,11 @@ import ImgPlant from "../../../Assets/Products/Plants.svg";
 import ImgBath from "../../../Assets/Products/Bathroom.svg";
 import ImgWall from "../../../Assets/Products/Wall.svg";
 import ImgSmart from "../../../Assets/Products/SmartHome.svg";
+import Linetop from "../../../Assets/Products/Line.svg";
 
 const Products = () => {
-  const [selectedCategory, setSelectedCategory] = useState("Furniture");
+  const [selectedCategory, setSelectedCategory] = useState("Furnitures");
+  const [selectedTypes, setSelectedTypes] = useState("Modular Furniture");
   const navigate = useNavigate();
 
   const ProductCategory = {
@@ -34,14 +36,14 @@ const Products = () => {
     Decor: [
       {
         img: ImgDecor,
-        title: "Decors",
+        title: "Decor",
         types: {
-          type1: "Modular Furniture",
-          type2: "Premium",
-          type3: "Ergonomic Office",
-          type4: "Tech-integrated",
-          type5: "Outdoor",
-          type6: "Storage",
+          type1: "Wall Art",
+          type2: "Sculptures",
+          type3: "Candles",
+          type4: "Vases",
+          type5: "Mirrors",
+          type6: "Rugs",
         },
       },
     ],
@@ -50,12 +52,12 @@ const Products = () => {
         img: ImgPlant,
         title: "Plants",
         types: {
-          type1: "Modular Furniture",
-          type2: "Premium",
-          type3: "Ergonomic Office",
-          type4: "Tech-integrated",
-          type5: "Outdoor",
-          type6: "Storage",
+          type1: "Indoor",
+          type2: "Outdoor",
+          type3: "Succulents",
+          type4: "Planters",
+          type5: "Artificial",
+          type6: "Hanging",
         },
       },
     ],
@@ -64,12 +66,12 @@ const Products = () => {
         img: ImgBath,
         title: "Bathroom & Wellness",
         types: {
-          type1: "Modular Furniture",
-          type2: "Premium",
-          type3: "Ergonomic Office",
-          type4: "Tech-integrated",
-          type5: "Outdoor",
-          type6: "Storage",
+          type1: "Towels",
+          type2: "Bath Mats",
+          type3: "Shower Accessories",
+          type4: "Storage",
+          type5: "Wellness Products",
+          type6: "Spa Essentials",
         },
       },
     ],
@@ -78,12 +80,12 @@ const Products = () => {
         img: ImgWall,
         title: "Wall & Flooring",
         types: {
-          type1: "Modular Furniture",
-          type2: "Premium",
-          type3: "Ergonomic Office",
-          type4: "Tech-integrated",
-          type5: "Outdoor",
-          type6: "Storage",
+          type1: "Wallpaper",
+          type2: "Tiles",
+          type3: "Wooden Panels",
+          type4: "Vinyl Flooring",
+          type5: "Paints",
+          type6: "Wall Stickers",
         },
       },
     ],
@@ -92,20 +94,32 @@ const Products = () => {
         img: ImgSmart,
         title: "Smart Home",
         types: {
-          type1: "Modular Furniture",
-          type2: "Premium",
-          type3: "Ergonomic Office",
-          type4: "Tech-integrated",
-          type5: "Outdoor",
-          type6: "Storage",
+          type1: "Lighting",
+          type2: "Security Systems",
+          type3: "Voice Assistants",
+          type4: "Smart Appliances",
+          type5: "Home Automation",
+          type6: "Sensors",
         },
       },
     ],
   };
+
   const categories = Object.keys(ProductCategory);
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    setSelectedTypes(ProductCategory[category][0].types.type1); // Default to the first type of the selected category
+  };
+
+  const handleTypeClick = (type) => {
+    setSelectedTypes(type);
+  };
+
   return (
     <div className="main">
-      <HomeNavbar></HomeNavbar>
+      <HomeNavbar />
+
       {/* Hero Section with Background */}
       <div
         className="services-container"
@@ -119,12 +133,163 @@ const Products = () => {
           </p>
         </div>
       </div>
+
+      {/* Horizontal Menu for Categories */}
       <div className="products-menu">
-        {categories.map((category) => {
-          <span></span>;
-        })}
+        {categories.map((category) => (
+          <div
+            key={category}
+            className={`product-card ${
+              selectedCategory === category ? "active" : ""
+            }`}
+            onClick={() => handleCategoryClick(category)}
+          >
+            <img
+              src={ProductCategory[category][0].img}
+              alt={ProductCategory[category][0].title}
+              className="product-image"
+            />
+            <span className="product-title">
+              {ProductCategory[category][0].title}
+            </span>
+          </div>
+        ))}
       </div>
-      <Footer></Footer>
+      {/* <Linetop></Linetop> */}
+      {/* Horizontal Menu for Types */}
+      <div className="types-menu">
+        {Object.values(ProductCategory[selectedCategory][0].types).map(
+          (type, index) => (
+            <span
+              key={index}
+              className={`type-item ${selectedTypes === type ? "active" : ""}`}
+              onClick={() => handleTypeClick(type)}
+            >
+              {type}
+            </span>
+          )
+        )}
+      </div>
+
+      <div className="p-4">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+          <div class="col">
+            <div class="card">
+              {/* <img src="..." class="card-img-top" alt="..."> */}
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              {/* <img src="..." class="card-img-top" alt="..."> */}
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              {/* <img src="..." class="card-img-top" alt="..."> */}
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              {/* <img src="..." class="card-img-top" alt="..."> */}
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className=" container similar-products ">
+        <h2>Similar Products</h2>
+        <div>
+          {" "}
+          <button>Veiw More</button>
+        </div>
+      </div>
+      <div className="p-4">
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+          <div class="col">
+            <div class="card">
+              {/* <img src="..." class="card-img-top" alt="..."> */}
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              {/* <img src="..." class="card-img-top" alt="..."> */}
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              {/* <img src="..." class="card-img-top" alt="..."> */}
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              {/* <img src="..." class="card-img-top" alt="..."> */}
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
