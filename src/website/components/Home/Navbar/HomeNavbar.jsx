52e751afa91b564dfda7e1a61ce55bc00eb95476
dev-dennis-d <dennisdaniel2002@gmail.com>
2025-03-21 16:06:40 +0530

    new changes

--- src/website/components/Home/Navbar/HomeNavbar.jsx
+++ src/website/components/Home/Navbar/HomeNavbar.jsx
@@ -8,11 +8,17 @@ const HomeNavbar = () => {
       style={{
         padding: "clamp(20px, 3vw, 36px) clamp(10px, 5vw, 100px)", // Adjusted min padding for small screens
         backgroundColor: "#FCF9F5",
+        background: "transparent",
+        width: "100%",
+        position: "absolute",
+        // top: "0",
+        // left: "0",
+        zIndex: 1000,
       }}
     >
       <div className="container-fluid">
         {/* Left: Logo */}
-        <a className="navbar-brand d-flex align-items-center" href="#">
+        <a className="d-flex navbar-brand align-items-center" href="#">
           <img
             src={pic}
             alt="Logo"
@@ -25,7 +31,7 @@ const HomeNavbar = () => {
 
         {/* Navbar Toggler for Mobile */}
         <button
-          className="navbar-toggler d-lg-none"
+          className="d-lg-none navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -46,7 +52,7 @@ const HomeNavbar = () => {
           className="collapse navbar-collapse justify-content-center"
           id="navbarSupportedContent"
         >
-          <ul className="navbar-nav d-flex align-items-center gap-4">
+          <ul className="d-flex navbar-nav align-items-center gap-4">
             {["About us", "Products", "Projects", "Services"].map(
               (item, index) => (
                 <li className="nav-item" key={index}>
@@ -54,7 +60,7 @@ const HomeNavbar = () => {
                     className="nav-link"
                     href="#"
                     style={{
-                      color: "#4C6559",
+                      color: "white",
                       fontFamily: "Kollektif, sans-serif",
                       fontWeight: 400,
                       fontSize: "clamp(16px, 2vw, 20px)",
@@ -71,7 +77,7 @@ const HomeNavbar = () => {
           </ul>
 
           {/* Contact Us Button (Hidden on large screens, shown inside menu on mobile) */}
-          <div className="d-lg-none mt-3 text-center">
+          <div className="d-lg-none text-center mt-3">
             <button
               className="btn w-40"
               style={{
@@ -92,11 +98,11 @@ const HomeNavbar = () => {
 
         {/* Right: Contact Us Button (Only visible on large screens) */}
         <button
-          className="btn d-none d-lg-block"
+          className="d-lg-block d-none btn"
           style={{
-            color: "white",
+            color: "#4C6559",
             border: "1px solid #4C6559",
-            background: "#4C6559",
+            background: "white",
             fontFamily: "Kollektif, sans-serif",
             fontWeight: 400,
             fontSize: "clamp(16px, 2vw, 20px)",
